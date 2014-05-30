ac2a33b73a053f975f87eb6a331af045efabdb3c
niket <niketkumar@gmail.com>
2014-05-30 14:41:12 +0530

    More readable code

--- src/Chapter5/Problems.hs
+++ src/Chapter5/Problems.hs
@@ -3,5 +3,7 @@ module Chapter5.Problems where
 primes :: [Integer]
 primes = primes' 2 (dropMultiples 2 [2..]) 
          where 
-             primes' p (p':ns) = p : primes' p' (dropMultiples p' ns)
+             primes' p pns = let p' = head pns
+                                 ns = tail pns
+                             in p : primes' p' (dropMultiples p' ns)
              dropMultiples p = filter (\i -> i `mod` p /= 0)
